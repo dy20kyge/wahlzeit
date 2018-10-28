709c447ac3981a9383edf234957fc798eb42df3b
Sören <Sren@192.168.178.42>
2018-10-28 16:28:47 +0100

    Addes Test cases

---
+++ src/test/java/org/wahlzeit/model/LocationTest.java
@@ -0,0 +1,23 @@
+package org.wahlzeit.model;
+
+import org.junit.Test;
+
+public class LocationTest{
+	
+	@Test
+	public void testSetAndGet() {
+		Location loc = new Location();
+		Coordinate c = new Coordinate(0, 0, 1);
+		loc.setCoordinate(c);
+		Coordinate check = loc.getCoordinate();
+		assertEquals(check, c);
+	}
+	
+	@Test
+	public void testEquals() {
+		Coordinate c0 = new Coordinate(0, 0, 1);
+		Coordinate c1 = new Coordinate(0, 0, 1);
+		assertTrue(c0.isEqual(c1));
+	}
+	
+}
