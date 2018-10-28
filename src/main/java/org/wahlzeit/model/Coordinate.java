7b0101b8162aac1370eae9d8f24bc6c9117a6c22
Sören <Sren@10.21.17.97>
2018-10-28 12:55:13 +0100

    Added get/set for coordinate

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -9,5 +9,48 @@ public class Coordinate{
 	protected double x;
 	protected double y;
 	protected double z;
+
 	
+	
+	/**
+	 * @methodtype set
+	 */
+	public void setX(double xValue) {
+		this.x = xValue;
+	}
+	
+	/**
+	 * @methodtype set
+	 */
+	public void setY(double yValue) {
+		this.y = yValue;
+	}
+	
+	/**
+	 * @methodtype set
+	 */
+	public void setZ(double zValue) {
+		this.z = zValue;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public double getX() {
+		return this.x;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public double getY() {
+		return this.y;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public double getZ() {
+		return this.z;
+	}
 }
--- src/main/java/org/wahlzeit/model/Location.java
+++ src/main/java/org/wahlzeit/model/Location.java
@@ -3,5 +3,5 @@ package org.wahlzeit.model;
 
 public class Location{
 	
-	
+	protected Coordinate coordinate;
 }
