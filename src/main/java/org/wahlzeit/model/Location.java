9c5252efad1bc4a02b538c7b06419a0d18333310
Sören <Sren@192.168.178.42>
2018-10-28 13:52:25 +0100

    Added get/set in Location for Coordinate, added equals

--- src/main/java/org/wahlzeit/model/Location.java
+++ src/main/java/org/wahlzeit/model/Location.java
@@ -4,4 +4,23 @@ package org.wahlzeit.model;
 public class Location{
 	
 	protected Coordinate coordinate;
+	
+	
+	/**
+	 * @methodtype set
+	 */
+	public void setCoordinate(Coordinate c) {
+		this.coordinate = c;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public Coordinate getCoordinate() {
+		return coordinate;
+	}
+	
+	public boolean equals(Location loc) {
+		return this.coordinate.isEqual(loc.coordinate);
+	}
 }
