f1ce74c0d6417031ab9f49a208f1fce4d71eedc5
Sören <Sren@Sren-PC>
2018-11-11 23:07:03 +0100

    Created LogoPhotoManager

---
+++ src/main/java/org/wahlzeit/model/LogoPhotoManager.java
@@ -0,0 +1,173 @@
+
+package org.wahlzeit.model;
+
+public class LogoPhotoManager extends PhotoManager{
+	
+	protected static final PhotoManager instance = new LogoPhotoManager();
+	
+	protected Map<PhotoId, LogoPhoto> photoCache = new HashMap<PhotoId, LogoPhoto>();
+	
+	public LogoPhotoManager() {
+		photoTagCollector = LogoPhotoFactory.getInstance().createPhotoTagCollector();
+	}
+	
+	/**
+	 * @methodtype command
+	 *
+	 * Loads all scaled Images of this Photo from Google Cloud Storage
+	 */
+	@Override
+	protected void loadScaledImages(LogoPhoto photo) {
+		String photoIdAsString = photo.getId().asString();
+		ImageStorage imageStorage = ImageStorage.getInstance();
+
+		for (PhotoSize photoSize : PhotoSize.values()) {
+			log.config(LogBuilder.createSystemMessage().
+					addAction("loading image").
+					addParameter("image size", photoSize.asString()).
+					addParameter("photo ID", photoIdAsString).toString());
+			if (imageStorage.doesImageExist(photoIdAsString, photoSize.asInt())) {
+				try {
+					Serializable rawImage = imageStorage.readImage(photoIdAsString, photoSize.asInt());
+					if (rawImage != null && rawImage instanceof Image) {
+						photo.setImage(photoSize, (Image) rawImage);
+					}
+				} catch (IOException e) {
+					log.warning(LogBuilder.createSystemMessage().
+							addParameter("size", photoSize.asString()).
+							addParameter("photo ID", photoIdAsString).
+							addException("Could not load image although it exists", e).toString());
+				}
+			} else {
+				log.config(LogBuilder.createSystemMessage().
+						addParameter("Size does not exist", photoSize.asString()).toString());
+			}
+		}
+	}
+
+	/**
+	 *
+	 */
+	@Override
+	public void savePhoto(LogoPhoto photo) {
+		updateObject(photo);
+	}
+	
+	@Override
+	protected void updateDependents(Persistent obj) {
+		if (obj instanceof LogoPhoto) {
+			LogoPhoto photo = (LogoPhoto) obj;
+			saveScaledImages(photo);
+			updateTags(photo);
+			UserManager userManager = UserManager.getInstance();
+			Client owner = userManager.getClientById(photo.getOwnerId());
+			userManager.saveClient(owner);
+		}
+	}
+	
+	/**
+	 * @methodtype command
+	 *
+	 * Persists all available sizes of the Photo. If one size exceeds the limit of the persistence layer, e.g. > 1MB for
+	 * the Datastore, it is simply not persisted.
+	 */
+	@Override
+	protected void saveScaledImages(LogoPhoto photo) {
+		String photoIdAsString = photo.getId().asString();
+		ImageStorage imageStorage = ImageStorage.getInstance();
+		PhotoSize photoSize;
+		int it = 0;
+		boolean moreSizesExist = true;
+		do{
+			photoSize = PhotoSize.values()[it];
+			it++;
+			Image image = photo.getImage(photoSize);
+			if (image != null) {
+				try {
+					if (!imageStorage.doesImageExist(photoIdAsString, photoSize.asInt())) {
+						imageStorage.writeImage(image, photoIdAsString, photoSize.asInt());
+					}
+				} catch (Exception e) {
+					log.warning(LogBuilder.createSystemMessage().
+							addException("Problem when storing image", e).toString());
+					moreSizesExist = false;
+				}
+			} else {
+				log.config(LogBuilder.createSystemMessage().
+						addParameter("No image for size", photoSize.asString()).toString());
+				moreSizesExist = false;
+			}
+		} while (it < PhotoSize.values().length && moreSizesExist);
+	}
+	
+	/**
+	 * Removes all tags of the Photo (obj) in the datastore that have been removed by the user and adds all new tags of
+	 * the photo to the datastore.
+	 */
+	@Override
+	protected void updateTags(LogoPhoto photo) {
+		// delete all existing tags, for the case that some have been removed
+		deleteObjects(Tag.class, Tag.PHOTO_ID, photo.getId().asString());
+
+		// add all current tags to the datastore
+		Set<String> tags = new HashSet<String>();
+		photoTagCollector.collect(tags, photo);
+		for (Iterator<String> i = tags.iterator(); i.hasNext(); ) {
+			Tag tag = new Tag(i.next(), photo.getId().asString());
+			log.config(LogBuilder.createSystemMessage().addParameter("Writing Tag", tag.asString()).toString());
+			writeObject(tag);
+		}
+	}
+
+	/**
+	 * @methodtype get
+	 */
+	@Override
+	public Map<PhotoId, LogoPhoto> getPhotoCache() {
+		return photoCache;
+	}
+
+	/**
+	 *
+	 */
+	public Set<LogoPhoto> findPhotosByOwner(String ownerName) {
+		Set<Photo> result = new HashSet<LogoPhoto>();
+		readObjects(result, LogoPhoto.class, LogoPhoto.OWNER_ID, ownerName);
+
+		for (Iterator<LogoPhoto> i = result.iterator(); i.hasNext(); ) {
+			doAddPhoto(i.next());
+		}
+
+		return result;
+	}
+
+	/**
+	 *
+	 */
+	public LogoPhoto getVisiblePhoto(PhotoFilter filter) {
+		filter.generateDisplayablePhotoIds();
+		return getPhotoFromId(filter.getRandomDisplayablePhotoId());
+	}
+
+	/**
+	 *
+	 */
+	public LogoPhoto createPhoto(String filename, Image uploadedImage) throws Exception {
+		PhotoId id = PhotoId.getNextId();
+		LogoPhoto result = PhotoUtil.createPhoto(filename, id, uploadedImage);
+		addPhoto(result);
+		return result;
+	}
+
+	/**
+	 * @methodtype command
+	 */
+	public void addPhoto(LogoPhoto photo) throws IOException {
+		PhotoId id = photo.getId();
+		assertIsNewPhoto(id);
+		doAddPhoto(photo);
+
+		GlobalsManager.getInstance().saveGlobals();
+	}
+	
+}
