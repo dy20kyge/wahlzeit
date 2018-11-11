da99218673c8e15991b3c980725e66aa2d6d8fd0
Sören <Sren@Sren-PC>
2018-11-12 00:31:52 +0100

    typo

--- src/main/java/org/wahlzeit/main/ModelMain.java
+++ src/main/java/org/wahlzeit/main/ModelMain.java
@@ -116,7 +116,7 @@ public abstract class ModelMain extends AbstractMain {
 			//TODO: change to datastore/cloud storage
 			try {
 				Image image = getImageFromFile(photo);
-				Photo newPhoto = LogoPhotoManager.createPhoto(photo.getName(), image);
+				Photo newPhoto = photoManager.createPhoto(photo.getName(), image);
 				user.addPhoto(newPhoto);
 				userManager.addClient(user);
 			} catch (Exception e) {
