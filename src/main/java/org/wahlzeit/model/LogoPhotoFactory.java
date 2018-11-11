576bf22151e3824dc0d3eabe4eddbd0ab5935a65
Sören <Sren@Sren-PC>
2018-11-11 21:36:44 +0100

    Created LogoPhotoFactory
    Added methods from PhotoFactory

--- src/main/java/org/wahlzeit/model/LogoPhotoFactory.java
+++ src/main/java/org/wahlzeit/model/LogoPhotoFactory.java
@@ -1,6 +1,67 @@
 
 package org.wahlzeit.model;
 
+import org.wahlzeit.services.LogBuilder;
+import java.util.logging.Logger;
+
 public class LogoPhotoFactory extends PhotoFactory {
+	
+	private static LogoPhotoFactory instance = null;
+	
+	/**
+	 * @methodtype constructor
+	 */
+	protected LogoPhotoFactory() {
+		super()
+	}
+	
+	/*
+	 * Acccess method for singleton
+	 */
+	/**
+	 * @methodtype get
+	 */
+	public static synchronized LogoPhotoFactory getInstance() {
+		if(instance == null) {
+			setInstance(new LogoPhotoFactory());
+		}
+		
+		return instance;
+	}
+	
+	/**
+	 * @methodtype set
+	 */
+	protected static synchronized setInstance(LogoPhotoFactory lpf) {
+		if (instance != null) {
+			throw new IllegalStateException("attempt to initalize PhotoFactory twice");
+		}
+
+		instance = lpf;
+	}
+	
+	/**
+	 * @methodtype factory
+	 */
+	@Override
+	public LogoPhoto createPhoto() {
+		return new LogoPhoto();
+	}
+
+	/**
+	 * Creates a new photo with the specified id
+	 */
+	@Override
+	public LogoPhoto createPhoto(PhotoId id) {
+		return new Photo(id);
+	}
+	
+	@Override
+	public LogoPhoto loadPhoto(PhotoId id) {
+		return null;
+	}
+	
+	
+	
 	
 }
