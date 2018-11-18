ed66c627748befda0e790234c3ce799829d0ec23
Sören <Sren@192.168.178.57>
2018-11-19 00:21:04 +0100

    bugfixes

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -4,9 +4,9 @@ package org.wahlzeit.model;
 public interface Coordinate{
 	
 	public CartesianCoordinate asCartesianCoordinate();
-	public double getCartesianDistance(Coordinate);
+	public double getCartesianDistance(Coordinate c);
 	public SphericCoordinate asSphericCoordinate();
-	public double getCentralAngle(Coordinate);
-	public boolean isEqual(Coordinate); 
+	public double getCentralAngle(Coordinate c);
+	public boolean isEqual(Coordinate c); 
 	
 }
