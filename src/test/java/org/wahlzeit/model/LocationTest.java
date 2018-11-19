56f9ba7c44b4e739e2c0a3544b96c5a455f6f3a8
Sören <Sren@192.168.178.57>
2018-11-19 09:43:21 +0100

    Added Test for isEqual

--- src/test/java/org/wahlzeit/model/LocationTest.java
+++ src/test/java/org/wahlzeit/model/LocationTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertTrue;
 public class LocationTest{
 	
 	@Test
-	public void testSetAndGet() {
+	public void testSetAndGetCartesian() {
 		Location loc = new Location();
 		CartesianCoordinate c = new CartesianCoordinate(0, 0, 1);
 		loc.setCoordinate(c);
@@ -16,24 +16,33 @@ public class LocationTest{
 	}
 	
 	@Test
-	public void testEquals() {
+	public void testEqualsCartesian() {
 		CartesianCoordinate c0 = new CartesianCoordinate(0, 0, 1);
 		CartesianCoordinate c1 = new CartesianCoordinate(0, 0, 1);
 		assertTrue(c0.isEqual(c1));
 	}
 	
 	@Test
-	public void testDistance() {
+	public void testDistanceCartesian() {
 		CartesianCoordinate c0 = new CartesianCoordinate(0, 0, 1);
 		CartesianCoordinate c1 = new CartesianCoordinate(0, 0, 0);
 		assertTrue(c0.getCartesianDistance(c1) == 1.0);
 	}
 	
 	@Test
-	public void testDistance2() {
+	public void testDistance2Cartesian() {
 		CartesianCoordinate c0 = new CartesianCoordinate(2, 2, 1);
 		CartesianCoordinate c1 = new CartesianCoordinate(0, 0, 0);
 		assertTrue(c0.getCartesianDistance(c1) ==3.0);
+	}
+	
+	@Test
+	public void asCartesianTest() {
+		CartesianCoordinate checkCoordinate = new CartesianCoordinate(1.0, 0.0, 0.0);
+		Coordinate test1 = new CartesianCoordinate(1.0, 0.0, 0.0);
+		Coordinate test2 = new SphericCoordinate(0.0, 0.0, 1.0);
+		assertTrue(checkCoordinate.isEqual(test1));
+		assertTrue(checkCoordinate.isEqual(test2));
 	}
 	
 }
