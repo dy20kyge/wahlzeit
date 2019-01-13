d40146539d0276995f0a0c9c4b50688f42a66fd2
Sören <Sren@192.168.178.42>
2019-01-13 23:25:13 +0100

    Changes for ValueObjects
    *changed the constrution methods in the test cases

--- src/test/java/org/wahlzeit/model/LocationTest.java
+++ src/test/java/org/wahlzeit/model/LocationTest.java
@@ -3,12 +3,13 @@ package org.wahlzeit.model;
 import org.junit.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import org.wahlzeit.model.CartesianCoordinate;
 
 public class LocationTest{
 	
 	@Test
 	public void testSetAndGetCartesian() {
-		CartesianCoordinate c = new CartesianCoordinate(0, 0, 1);
+		CartesianCoordinate c = CartesianCoordinate.createCartesianCoordinate(0, 0, 1);
 		Location loc = new Location(c);
 		CartesianCoordinate check = (CartesianCoordinate) loc.getCoordinate();
 		assertEquals(check, c);
@@ -16,36 +17,36 @@ public class LocationTest{
 	
 	@Test
 	public void testEqualsCartesian() {
-		CartesianCoordinate c0 = new CartesianCoordinate(0, 0, 1);
-		CartesianCoordinate c1 = new CartesianCoordinate(0, 0, 1);
+		CartesianCoordinate c0 = CartesianCoordinate.createCartesianCoordinate(0, 0, 1);
+		CartesianCoordinate c1 = CartesianCoordinate.createCartesianCoordinate(0, 0, 1);
 		assertTrue(c0.isEqual(c1));
 	}
 	
 	@Test
 	public void testDistanceCartesian() {
-		CartesianCoordinate c0 = new CartesianCoordinate(0, 0, 1);
-		CartesianCoordinate c1 = new CartesianCoordinate(0, 0, 0);
+		CartesianCoordinate c0 = CartesianCoordinate.createCartesianCoordinate(0, 0, 1);
+		CartesianCoordinate c1 = CartesianCoordinate.createCartesianCoordinate(0, 0, 0);
 		assertTrue(c0.getCartesianDistance(c1) == 1.0);
 	}
 	
 	@Test
 	public void testDistance2Cartesian() {
-		CartesianCoordinate c0 = new CartesianCoordinate(2, 2, 1);
-		CartesianCoordinate c1 = new CartesianCoordinate(0, 0, 0);
+		CartesianCoordinate c0 = CartesianCoordinate.createCartesianCoordinate(2, 2, 1);
+		CartesianCoordinate c1 = CartesianCoordinate.createCartesianCoordinate(0, 0, 0);
 		assertTrue(c0.getCartesianDistance(c1) ==3.0);
 	}
 	
 	@Test
 	public void testAsCartesianAndIsEqual() {
-		CartesianCoordinate checkCoordinate = new CartesianCoordinate(0.0, 0.0, 0.0);
-		Coordinate test1 = new CartesianCoordinate(0.0, 0.0, 0.0);
-		Coordinate test2 = new SphericCoordinate(0.0, 0.0, 0.0);
+		CartesianCoordinate checkCoordinate = CartesianCoordinate.createCartesianCoordinate(0.0, 0.0, 0.0);
+		Coordinate test1 = CartesianCoordinate.createCartesianCoordinate(0.0, 0.0, 0.0);
+		Coordinate test2 = SphericCoordinate.createSphericCoordinate(0.0, 0.0, 0.0);
 		CartesianCoordinate test1C = (CartesianCoordinate) test1;
 		assertTrue(checkCoordinate.isEqual(test1C));
 		CartesianCoordinate test2C = test2.asCartesianCoordinate();
 		assertTrue(checkCoordinate.isEqual(test2C));
-		checkCoordinate = new CartesianCoordinate(0.0, 0.0, 1.0);
-		Coordinate test3 = new SphericCoordinate(0.0, 0.0, 1.0);
+		checkCoordinate = CartesianCoordinate.createCartesianCoordinate(0.0, 0.0, 1.0);
+		Coordinate test3 = SphericCoordinate.createSphericCoordinate(0.0, 0.0, 1.0);
 		CartesianCoordinate test3C = test3.asCartesianCoordinate();
 		assertTrue(checkCoordinate.isEqual(test3C));
 
@@ -53,31 +54,31 @@ public class LocationTest{
 	
 	@Test
 	public void testIsEqual() {
-		CartesianCoordinate checkCoordinateC = new CartesianCoordinate(0.0, 0.0, 0.0);
-		SphericCoordinate checkCoordinateS = new SphericCoordinate(0.0, 0.0, 0.0);
-		Coordinate test1 = new CartesianCoordinate(0.0, 0.0, 0.0);
-		Coordinate test2 = new SphericCoordinate(0.0, 0.0, 0.0);
+		CartesianCoordinate checkCoordinateC = CartesianCoordinate.createCartesianCoordinate(0.0, 0.0, 0.0);
+		SphericCoordinate checkCoordinateS = SphericCoordinate.createSphericCoordinate(0.0, 0.0, 0.0);
+		Coordinate test1 = CartesianCoordinate.createCartesianCoordinate(0.0, 0.0, 0.0);
+		Coordinate test2 = SphericCoordinate.createSphericCoordinate(0.0, 0.0, 0.0);
 		assertTrue(checkCoordinateC.isEqual(test1));
 		assertTrue(checkCoordinateS.isEqual(test2));
 		
-		checkCoordinateC = new CartesianCoordinate(0.0, 0.0, 1.0);
-		checkCoordinateS = new SphericCoordinate(0.0, 0.0, 1.0);
-		Coordinate test3 = new SphericCoordinate(0.0, 0.0, 1.0);
+		checkCoordinateC = CartesianCoordinate.createCartesianCoordinate(0.0, 0.0, 1.0);
+		checkCoordinateS = SphericCoordinate.createSphericCoordinate(0.0, 0.0, 1.0);
+		Coordinate test3 = SphericCoordinate.createSphericCoordinate(0.0, 0.0, 1.0);
 		assertTrue(checkCoordinateC.isEqual(test3));
 		assertTrue(checkCoordinateS.isEqual(test3));
 	}
 	
 	@Test
 	public void testAsSphericAndIsEqual() {
-		SphericCoordinate checkCoordinate = new SphericCoordinate(0.0, 0.0, 0.0);
-		Coordinate test1 = new CartesianCoordinate(0.0, 0.0, 0.0);
-		Coordinate test2 = new SphericCoordinate(0.0, 0.0, 0.0);
+		SphericCoordinate checkCoordinate = SphericCoordinate.createSphericCoordinate(0.0, 0.0, 0.0);
+		Coordinate test1 = CartesianCoordinate.createCartesianCoordinate(0.0, 0.0, 0.0);
+		Coordinate test2 = SphericCoordinate.createSphericCoordinate(0.0, 0.0, 0.0);
 		SphericCoordinate test1S = test1.asSphericCoordinate();
 		assertTrue(checkCoordinate.isEqual(test1S));
 		SphericCoordinate test2S = (SphericCoordinate) test2;
 		assertTrue(checkCoordinate.isEqual(test2S));
-		checkCoordinate = new SphericCoordinate(0.0, 0.0, 1.0);
-		Coordinate test3 = new CartesianCoordinate(0.0, 0.0, 1.0);
+		checkCoordinate = SphericCoordinate.createSphericCoordinate(0.0, 0.0, 1.0);
+		Coordinate test3 = CartesianCoordinate.createCartesianCoordinate(0.0, 0.0, 1.0);
 		SphericCoordinate test3S = test3.asSphericCoordinate();
 		assertTrue(checkCoordinate.isEqual(test3S));
 
@@ -85,10 +86,10 @@ public class LocationTest{
 	
 	@Test
 	public void testGetCartesianDistance() {
-		Coordinate sphericA = new SphericCoordinate(0.0, 0.0, 0.0);
-		Coordinate sphericB = new SphericCoordinate(0.0, 0.0, 1.0);
-		Coordinate cartesianA = new CartesianCoordinate(0.0, 0.0, 0.0);
-		Coordinate cartesianB = new CartesianCoordinate(0.0, 0.0, 1.0);
+		Coordinate sphericA = SphericCoordinate.createSphericCoordinate(0.0, 0.0, 0.0);
+		Coordinate sphericB = SphericCoordinate.createSphericCoordinate(0.0, 0.0, 1.0);
+		Coordinate cartesianA = CartesianCoordinate.createCartesianCoordinate(0.0, 0.0, 0.0);
+		Coordinate cartesianB = CartesianCoordinate.createCartesianCoordinate(0.0, 0.0, 1.0);
 		
 		assertTrue(sphericA.getCartesianDistance(sphericA) == 0.0);
 		assertTrue(sphericA.getCartesianDistance(sphericB) == 1.0);
@@ -108,8 +109,8 @@ public class LocationTest{
 	
 	@Test
 	public void testGetCentralAngle() {
-		Coordinate sphericA = new SphericCoordinate(0.0, 0.0, 0.0);
-		Coordinate cartesianA = new CartesianCoordinate(0.0, 0.0, 0.0);
+		Coordinate sphericA = SphericCoordinate.createSphericCoordinate(0.0, 0.0, 0.0);
+		Coordinate cartesianA = CartesianCoordinate.createCartesianCoordinate(0.0, 0.0, 0.0);
 		
 		assertTrue(sphericA.getCentralAngle(cartesianA) == 0);
 		assertTrue(cartesianA.getCentralAngle(sphericA) == 0);
@@ -117,7 +118,7 @@ public class LocationTest{
 	
 	@Test
 	public void testValuesCartesianCoordinate() {
-		CartesianCoordinate origin = new CartesianCoordinate(0,0,0);
+		CartesianCoordinate origin = CartesianCoordinate.createCartesianCoordinate(0,0,0);
 		origin = origin.setX(3);
 		assertTrue(origin.getX() == 3);
 		origin = origin.setY(3);
@@ -132,7 +133,7 @@ public class LocationTest{
 	
 	@Test
 	public void testValuesSphericCoordinate() {
-		SphericCoordinate origin = new SphericCoordinate(0,0,0);
+		SphericCoordinate origin = SphericCoordinate.createSphericCoordinate(0,0,0);
 		origin = origin.setPhi(90);
 		assertTrue(origin.getPhi() == 90);
 		origin = origin.setTheta(75);
@@ -146,7 +147,7 @@ public class LocationTest{
 	
 	@Test
 	public void testValueObjectCartesian() {
-		CartesianCoordinate origin = new CartesianCoordinate(0,0,0);
+		CartesianCoordinate origin = CartesianCoordinate.createCartesianCoordinate(0,0,0);
 		CartesianCoordinate altered;
 		
 		altered = origin.setX(3);
@@ -172,7 +173,7 @@ public class LocationTest{
 	
 	@Test
 	public void testValueObjectSpheric() {
-		SphericCoordinate origin = new SphericCoordinate(0,0,0);
+		SphericCoordinate origin = SphericCoordinate.createSphericCoordinate(0,0,0);
 		SphericCoordinate altered;
 		
 		altered = origin.setPhi(90);
